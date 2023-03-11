428588e2cf789460cc52fbc28e715f0308e65686
Boat"   Sasipatr Siripairoj <125846563+sspsi77@users.noreply.github.com>
2023-03-11 16:49:35 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Challenge 1: Create an Age Calculator
+
+const myBirthYear = 1991;
+const futureYear = 2026;
+let myAge = futureYear - myBirthYear;
+console.log(myAge);
+
+// Challenge 2: Create an information card
+
+const myname = 'Boat';
+const location = 'Bangkok';
+const hobby = 'cooking at home';
+console.log(`" Hi, my name is ${myname}. I live in ${location} and enjoy ${hobby}." `);
